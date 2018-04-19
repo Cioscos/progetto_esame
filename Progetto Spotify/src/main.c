c51d04aa0e33975969f3cdc30b64f67e4e87527d
Cioscos <claudiocicca1998@gmail.com>
2018-04-19 18:42:20 +0200

    Inizio progetto

---
+++ Progetto Spotify/src/main.c
@@ -0,0 +1,16 @@
+/*
+*Created By: claud
+*18:41:19
+*19 apr 2018
+*main.c
+*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <conio.h>
+
+int main(int argc, char *argv[]){
+
+
+getch();
+return 0;
+}
