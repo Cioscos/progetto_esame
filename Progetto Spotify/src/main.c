fdb0c07967262410ba13d632e69b4061b445713c
ilMatty98 <ilmatty98s@gmail.com>
2018-04-22 22:07:35 +0200

    asddddddddddddddddd

--- Progetto Spotify/src/main.c
+++ Progetto Spotify/src/main.c
@@ -6,7 +6,6 @@
 
 
 
-
 int main(int argc, char *argv[]){
 
 	char input_utente[LUNGHEZZA_MAX]={"1"};		//Variabile contenente l'input dell'utente
@@ -18,43 +17,43 @@ int main(int argc, char *argv[]){
 			controllo_menu(input_utente,MENU_PRINCIPALE);
 		}while(isControllo_Numero(input_utente)!=1);		//Controllo sull'input dell'utente fin quando non viene digitato una cifra
 
+			switch(atoi(input_utente))		//Funzione atoi converta da stringa a intero
+			{
 
-		switch(atoi(input_utente))		//Funzione atoi converta da stringa a intero
-		{
-
-			case 0:	system("cls");
-					logo();
-					printf("Programma terminato\n");
-					break;
-
-			case 1:	system("cls");
-					logo();
-					flag=1;
-					do{
-						controllo_menu(input_utente,MENU_ARTISTA);
-					}while(isControllo_Numero(input_utente)!=1);		//Controllo sull'input dell'utente fin quando non viene digitato una cifra
-					system("PAUSE");
-					break;
-
-			case 2:	system("cls");
-					logo();
-					flag=1;
-					do{
-						controllo_menu(input_utente,MENU_UTENTE);
-					}while(isControllo_Numero(input_utente)!=1);		//Controllo sull'input dell'utente fin quando non viene digitato una cifra
-					system("PAUSE");
-					break;
-
-			case 3:	system("cls");
-					printf("Hai selezionato 3\n");
-					system("PAUSE");
-					break;
-
-			default : 	system("cls");
+				case 0:	system("cls");		//0000=0
 						logo();
-						printf("Comando errato, inserisci un valore corretto\a\n");
+						printf("Programma terminato\n");
+						break;
+
+				case 1:	system("cls");
+						logo();
+						flag=1;
+						do{
+							controllo_menu(input_utente,MENU_ARTISTA);
+						}while(isControllo_Numero(input_utente)!=1);		//Controllo sull'input dell'utente fin quando non viene digitato una cifra
 						system("PAUSE");
-		}
+						break;
+
+				case 2:	system("cls");
+						logo();
+						flag=1;
+						do{
+							controllo_menu(input_utente,MENU_UTENTE);
+						}while(isControllo_Numero(input_utente)!=1);		//Controllo sull'input dell'utente fin quando non viene digitato una cifra
+						system("PAUSE");
+						break;
+
+				case 3:	system("cls");
+						printf("Hai selezionato 3\n");
+						system("PAUSE");
+						break;
+
+				default : 	system("cls");
+							logo();
+							printf("Comando errato, inserisci un valore corretto\a\n");
+							system("PAUSE");
+			}
+
 	}
 
 
